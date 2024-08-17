863f64efc285fd50b8cd182e90612d5741aa7d8b
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-17 16:57:33 -0300

    Update ft_putchar.c

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,11 +1,6 @@
 int main() {
  int y #largura 
  int x #tamanho
- int read_keybord_1; #leitura 1
- int read_keybord_2; #leitura 2 
-
- scanf("d%", read_keybord_1)
- scanf("d%", read_keybord_2)
 
 
  if (x <= 0 &&  y <= 0){
