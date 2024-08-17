eaedd5ec0ac4ad9c7d330a2e45dc82ad9f26d379
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-17 16:35:52 -0300

    Create ft_putchar.c
    fazendo

---
+++ ft_putchar.c
@@ -0,0 +1,27 @@
+int main() {
+ int y #largura 
+ int x #tamanho
+ int read_keybord_1; #leitura 1
+ int read_keybord_2; #leitura 2 
+
+ scanf("d%", read_keybord_1)
+ scanf("d%", read_keybord_2)
+
+
+ if (x <= 0 &&  y <= 0){
+    printf("pls whith a positive number");
+ 
+}
+   
+while(int y = 0; y < read_keybord_1; y++) {
+
+
+
+
+
+}
+
+
+
+
+}
