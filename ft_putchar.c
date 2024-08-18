72f5f21637816bd95435d12096d726a036e91c9e
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-18 16:08:24 -0300

    Update ft_putchar.c

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,26 +1,29 @@
+#include <unistd.h>
+
 int main() {
- int y #largura 
- int x #tamanho
+ int y = 0  #largura 
+ int x = 0  #tamanho
  int read_1
  int read_2
-
-
- if (read_1 <= 0 && read_2 <= 0){
+ char c = "😊"; 
+ char c = "😈";
+if (read_1 <= 0 && read_2 <= 0){
     
  
-}
+} //fecha if read_1 and_2
    
-while(int y = 0; y < read_1; y++) { while(int x = 0; x < read_2; x++)
+while(y <= read_1) {
+write(1, &c, 1); 
+y++;	
+
+while(x <= read_2){
+write(1, &b, 1);        
+x++;
+} //fecha do x 
+
+} //facha  y
 
 
 
 
-
-
-
-}
-
-
-
-
-}
+} //fecha man 
