1e05e21b68710713c73bbab0e69877a648a96a7d
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-18 16:11:58 -0300

    Update ft_putchar.c

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,17 +1,30 @@
 #include <unistd.h>
 
 int main() {
- int y = 0  #largura 
- int x = 0  #tamanho
- int read_1
- int read_2
- char c = "😊"; 
- char c = "😈";
+ int y = 0;  //fixo_0_1
+ int x = 0;  //fixo_0_2
+ int read_1; //var_Read_1
+ int read_2; //var_Read_2
+ char c = "😊"; //print1 
+ char b = "😈"; //print2
 if (read_1 <= 0 && read_2 <= 0){
     
  
 } //fecha if read_1 and_2
    
+
+scanf("%d", &read_1); //remove before fly
+scanf("%d", &read_2); //remove before fly
+
+
+
+
+
+
+
+
+
+
 while(y <= read_1) {
 write(1, &c, 1); 
 y++;	
@@ -25,5 +38,5 @@ x++;
 
 
 
-
+ return 0;
 } //fecha man 
