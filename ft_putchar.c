1def3c25cb40f5892a204409ab8d34376e022ef8
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-18 11:24:35 -0300

    Update ft_putchar.c

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,14 +1,18 @@
 int main() {
  int y #largura 
  int x #tamanho
+ int read_1
+ int read_2
 
 
- if (x <= 0 &&  y <= 0){
+ if (read_1 <= 0 && read_2 <= 0){
     
  
 }
    
-while(int y = 0; y < read_keybord_1; y++) {
+while(int y = 0; y < read_1; y++) { while(int x = 0; x < read_2; x++)
+
+
 
 
 
